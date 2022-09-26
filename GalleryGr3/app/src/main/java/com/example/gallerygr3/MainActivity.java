d73ac8dc6769cadc0ccf61b3a1ee9b1333e7db8d
20120364 <phamphuocsang2834@gmail.com>
2022-09-26 12:01:39 +0700

    first commit on branch

--- GalleryGr3/app/src/main/java/com/example/gallerygr3/MainActivity.java
+++ GalleryGr3/app/src/main/java/com/example/gallerygr3/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.gallerygr3;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends Activity {
+
+    TextView testText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testText=
     }
+
+    private void readMobileFolder() {
+
+    }
+
 }
