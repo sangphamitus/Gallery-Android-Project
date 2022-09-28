f22b258072e49288cd62e1ead4fa6df4acee3a1f
Hai Yen <yyen9319@gmail.com>
2022-09-29 01:08:09 +0700

    display images at the different layout. Link: https://ppsang.atlassian.net/browse/A2G-4

---
+++ GalleryGr3/app/src/main/java/com/example/gallerygr3/SelectedPicture.java
@@ -0,0 +1,31 @@
+package com.example.gallerygr3;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SelectedPicture extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_picture);
+
+        imageView = findViewById(R.id.imageView);
+        textView =findViewById(R.id.tvName);
+
+        Intent intent = getIntent();
+        if(intent.getExtras()!=null){
+            String selectedName = intent.getStringExtra("name");
+            int selectedImage = intent.getIntExtra("image", 0);
+
+            textView.setText(selectedName);
+            imageView.setImageResource(selectedImage);
+        }
+    }
+}
