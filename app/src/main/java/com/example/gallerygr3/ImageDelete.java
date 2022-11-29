5e4fd27c5c6cff5802f50ec7ab62048c448769ca
20120364 <phamphuocsang2834@gmail.com>
2022-11-29 14:42:22 +0700

    Done Task: https://ppsang.atlassian.net/browse/A2G-39

--- app/src/main/java/com/example/gallerygr3/ImageDelete.java
+++ app/src/main/java/com/example/gallerygr3/ImageDelete.java
@@ -2,13 +2,47 @@ package com.example.gallerygr3;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class ImageDelete extends Activity {
+    static Bitmap blurfast(Bitmap bmp, int radius) {
+        int w = bmp.getWidth();
+        int h = bmp.getHeight();
+        int[] pix = new int[w * h];
+        bmp.getPixels(pix, 0, w, 0, 0, w, h);
+
+        for(int r = radius; r >= 1; r /= 2) {
+            for(int i = r; i < h - r; i++) {
+                for(int j = r; j < w - r; j++) {
+                    int tl = pix[(i - r) * w + j - r];
+                    int tr = pix[(i - r) * w + j + r];
+                    int tc = pix[(i - r) * w + j];
+                    int bl = pix[(i + r) * w + j - r];
+                    int br = pix[(i + r) * w + j + r];
+                    int bc = pix[(i + r) * w + j];
+                    int cl = pix[i * w + j - r];
+                    int cr = pix[i * w + j + r];
+
+                    pix[(i * w) + j] = 0xFF000000 |
+                            (((tl & 0xFF) + (tr & 0xFF) + (tc & 0xFF) + (bl & 0xFF) + (br & 0xFF) + (bc & 0xFF) + (cl & 0xFF) + (cr & 0xFF)) >> 3) & 0xFF |
+                            (((tl & 0xFF00) + (tr & 0xFF00) + (tc & 0xFF00) + (bl & 0xFF00) + (br & 0xFF00) + (bc & 0xFF00) + (cl & 0xFF00) + (cr & 0xFF00)) >> 3) & 0xFF00 |
+                            (((tl & 0xFF0000) + (tr & 0xFF0000) + (tc & 0xFF0000) + (bl & 0xFF0000) + (br & 0xFF0000) + (bc & 0xFF0000) + (cl & 0xFF0000) + (cr & 0xFF0000)) >> 3) & 0xFF0000;
+                }
+            }
+        }
+        bmp.setPixels(pix, 0, w, 0, 0, w, h);
+        return bmp;
+    }
+
+
     public static boolean DeleteImage(String[] ListImage){
         boolean running=true;
         for (String file : ListImage)
@@ -16,15 +50,7 @@ public final class ImageDelete extends Activity {
             File fdel=new File(file);
             if(fdel.exists()){
 
-                try
-                {
-                    fdel.delete();
-                }
-                catch (Error e)
-                {
-                    running =false;
-                    return running;
-                }
+                fdel.delete();
             }
         }
         return running;
@@ -32,20 +58,13 @@ public final class ImageDelete extends Activity {
 
 
     //xử lí single image
+
     public static boolean DeleteImage(String image){
         boolean running=true;
         File fdel=new File(image);
         if(fdel.exists()){
 
-            try
-            {
-                fdel.delete();
-            }
-            catch (Error e)
-            {
-                running =false;
-                return running;
-            }
+            fdel.delete();
         }
         return running;
     }
